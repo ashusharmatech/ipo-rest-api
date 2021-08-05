05ec9890d97a59ca4c56d5bed86f61696d3a9f8c
Ashutosh Sharma <47886733+ashusharmatech@users.noreply.github.com>
2021-08-05 14:04:58 +0530

    rest api

---
+++ app.py
@@ -0,0 +1,23 @@
+import flask
+from flask import jsonify
+import requests
+import AdvancedHTMLParser
+import html_to_json
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    url = "https://www.ipowatch.in/p/ipo-grey-market-premium-latest-ipo-grey.html"
+    html_response = requests.get(url=url)
+    parser = AdvancedHTMLParser.AdvancedHTMLParser()
+    parser.parseStr(html_response.text)
+    table = parser.getElementsByTagName("table")[0].outerHTML
+    table = table.replace('<td','<th',4)
+    table = table.replace('/td>','/th>',4)
+    json = html_to_json.convert_tables(table)
+    print(json)
+    return jsonify(json)
+
+app.run()
